6855e5ad52c13dfc689e1ea82897d5096aa9e926
Andres Chavez <107895172+ChavitoCdm@users.noreply.github.com>
2024-08-08 20:08:57 -0300

    Add files via upload

--- src/pages/SongTable/SongTable.tsx
+++ src/pages/SongTable/SongTable.tsx
@@ -1,1 +1,30 @@
+import { useEffect } from "react";
+import "react-toastify/dist/ReactToastify.css";
+import TableSongs from "../../components/TableSongs/TableSongs";
+import { getSongs } from "../../services/api";
 
+const SongTable = () => {
+  useEffect(() => {
+    const fetchSongs = async () => {
+      try {
+        const response = await getSongs();
+
+        console.log(response);
+      } catch (error) {
+        console.error("Error fetching songs:", error);
+      } finally {
+        console.log("Fetching songs completed");
+      }
+    };
+
+    fetchSongs();
+  }, []);
+
+  return (
+    <div>
+      <TableSongs />
+    </div>
+  );
+};
+
+export default SongTable;
