06da2a0283ed474936de778b8f0c40b7bc7884d7
Andres Chavez <107895172+ChavitoCdm@users.noreply.github.com>
2024-08-08 20:11:22 -0300

    Add files via upload

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,1 +1,24 @@
+// src/components/NotFoundPage.tsx
+import React from "react";
+import { Button, Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
+const NotFoundPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <Container className="text-center my-5">
+      <h1>404 - Página no encontrada</h1>
+      <p>Lo sentimos, la página que estás buscando no existe.</p>
+      <Button variant="primary" onClick={handleGoHome}>
+        Volver a la página principal
+      </Button>
+    </Container>
+  );
+};
+
+export default NotFoundPage;
